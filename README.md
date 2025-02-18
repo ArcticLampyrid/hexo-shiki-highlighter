a30c15cdc3a7a52e2d0e36a720e54cd3ccd5978c
ArcticLampyrid <ArcticLampyrid@outlook.com>
2025-02-18 23:32:50 +0800

    feat: support advanced theme settings & custom lang

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Hexo v7.0.0+ is required, due to highlighter api and top-level await support.
 - Support code block with caption. \(You may need to style it yourself.\)
 
 ## Setup
+### Get Started
 First, install this package.
 
 ```bash
@@ -30,6 +31,64 @@ Finally, configure the options for shiki in your `_config.yml`.
 shiki:
   theme: "dark-plus"
   strip_indent: true # Strip the leading indent of the code block (default: true, just like the official highlighter)
+```
+
+### Dual/Multi Themes
+You can use dual/multi themes by setting the `theme` option to an object.
+
+```yaml
+shiki:
+  theme:
+    light: "light-plus"
+    dark: "dark-plus"
+    # ... more themes
+  default_color: "light" # The default color to use, use false to disable the default color (default: "light")
+```
+
+Thus the code block will be rendered like this:
+
+```html
+<!-- Default Color = light -->
+<span style="color:#0000FF;--shiki-dark:#569CD6;">int</span>
+<!-- Default Color = false -->
+ <span style="--shiki-dark:#569CD6;--shiki-light:#0000FF">int</span>
+```
+
+Then you can use css to choose the theme based on the user's preference.
+
+```css
+/* Example from https://shiki.style/guide/dual-themes */ 
+@media (prefers-color-scheme: dark) {
+  .shiki,
+  .shiki span {
+    color: var(--shiki-dark) !important;
+    background-color: var(--shiki-dark-bg) !important;
+    /* Optional, if you also want font styles */
+    font-style: var(--shiki-dark-font-style) !important;
+    font-weight: var(--shiki-dark-font-weight) !important;
+    text-decoration: var(--shiki-dark-text-decoration) !important;
+  }
+}
+```
+
+### Custom Themes
+You can add custom themes:
+
+```yaml
+shiki:
+  custom_themes:
+    - "relative/path/to/custom-theme.json"
+```
+
+The theme file should be a JSON file that contains a valid theme config. For example, [it should look like this](https://github.com/shikijs/textmate-grammars-themes/blob/main/packages/tm-themes/themes/dark-plus.json).
+
+### Custom Languages
+You can add custom languages:
+
+```yaml
+shiki:
+  custom_languages:
+    - "relative/path/to/custom-language.json"
 ```
 
 ## Usage
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
-import { bundledLanguages, createHighlighter } from "shiki";
+import { bundledLanguages, bundledThemes, createHighlighter, ShikiTransformer } from "shiki";
 import { h } from 'hastscript'
 import { escapeHTML, stripIndent } from "hexo-util";
 import Hexo from "hexo";
 import { HighlightOptions } from "hexo/dist/extend/syntax_highlight";
 import { fromHtml } from 'hast-util-from-html'
+import { promises as fs } from 'fs';
+import path from "path";
 
 export async function init(hexo: Hexo) {
     const config = Object.assign(
         {
-            theme: "dark-plus",
+            theme: "dark-plus" as string | object,
             strip_indent: true,
         },
         hexo.config.shiki || {}
     );
+    const themes = typeof config.theme === "object" ? Object.values(config.theme) : [config.theme];
+    const requiredBundledThemes = themes.filter(theme => !!bundledThemes[theme]);
     const highlighter = await createHighlighter(
         {
             langs: Object.keys(bundledLanguages),
-            themes: [config.theme],
+            themes: requiredBundledThemes,
         }
     );
+
+    // Load custom themes
+    if (config.custom_themes) {
+        for (const filePath of config.custom_themes) {
+            // load theme from file
+            const themeContent = await fs.readFile(path.resolve(hexo.base_dir, filePath), "utf-8");
+            await highlighter.loadTheme(JSON.parse(themeContent));
+        }
+    }
+
+    // Check if all themes are supported
+    const supportedThemes = highlighter.getLoadedThemes().reduce((acc, theme) => {
+        acc[theme] = true;
+        return acc;
+    });
+    if (themes.some(theme => !supportedThemes[theme])) {
+        console.error(`Some themes are not supported: ${themes.filter(theme => !supportedThemes[theme]).join(", ")}`);
+    }
+
+    // Load custom languages
+    if (config.custom_languages) {
+        for (const filePath of config.custom_languages) {
+            // load language from file
+            const langContent = await fs.readFile(path.resolve(hexo.base_dir, filePath), "utf-8");
+            await highlighter.loadLanguage(JSON.parse(langContent));
+        }
+    }
+
+    // Get all supported languages
     const supportedLanguages = highlighter.getLoadedLanguages().reduce((acc, lang) => {
         acc[lang] = true;
         return acc;
@@ -28,6 +61,7 @@ export async function init(hexo: Hexo) {
         plain: true,
         ansi: true,
     });
+
     const hexoHighlighter = (code: string, options: HighlightOptions) => {
         try {
             var code = config.strip_indent ? stripIndent(code) as string : code;
@@ -35,26 +69,36 @@ export async function init(hexo: Hexo) {
             if (!lang || !supportedLanguages[lang]) {
                 lang = "text";
             }
-            return highlighter.codeToHtml(code, {
-                lang,
-                theme: config.theme,
-                transformers: [
-                    {
-                        pre(node) {
-                            if (options.caption) {
-                                const captionContent = fromHtml(options.caption, { fragment: true });
-                                const captionDiv = h("div", { className: ["caption"] }, captionContent.children);
-                                node.children.unshift(captionDiv);
-                            }
-                        },
-                        line(node, line) {
-                            if (options.mark && options.mark.includes(line)) {
-                                this.addClassToHast(node, 'marked')
-                            }
-                        },
+            var transformers: ShikiTransformer[] = [
+                {
+                    pre(node) {
+                        if (options.caption) {
+                            const captionContent = fromHtml(options.caption, { fragment: true });
+                            const captionDiv = h("div", { className: ["caption"] }, captionContent.children);
+                            node.children.unshift(captionDiv);
+                        }
                     },
-                ]
-            });
+                    line(node, line) {
+                        if (options.mark && options.mark.includes(line)) {
+                            this.addClassToHast(node, 'marked')
+                        }
+                    },
+                },
+            ];
+            if (typeof config.theme === "object") {
+                return highlighter.codeToHtml(code, {
+                    lang,
+                    themes: config.theme,
+                    defaultColor: typeof config.default_color === "undefined" ? "light" : config.default_color,
+                    transformers
+                });
+            } else {
+                return highlighter.codeToHtml(code, {
+                    lang,
+                    theme: config.theme,
+                    transformers
+                });
+            }
         } catch (e) {
             console.error(e);
             return `<pre><code>${escapeHTML(code)}</code></pre>`;
